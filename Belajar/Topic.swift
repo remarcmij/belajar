ce2db90e72f64e771996a12ad7d16e37a83f4b34
Jim Cramer <remarcmij@gmail.com>
2016-06-09 12:25:22 +0200

    first commit/push to remote

---
+++ Belajar/Topic.swift
@@ -0,0 +1,25 @@
+//
+//  Topic.swift
+//  Belajar
+//
+//  Created by Jim Cramer on 09/06/16.
+//  Copyright © 2016 Belajar NL. All rights reserved.
+//
+
+import Foundation
+
+struct Topic {
+    var id: Int = -1
+    var filename: String
+    var publication: String
+    var chapter: String
+    var groupName: String
+    var sortIndex: Int
+    var title: String
+    var subtitle: String?
+    var author: String?
+    var publisher: String?
+    var pubDate: String?
+    var icon: String?
+    var lastModified: String
+}
---
+++ Belajar/TopicMapper.swift
@@ -0,0 +1,30 @@
+//
+//  TopicMapper.swift
+//  Belajar
+//
+//  Created by Jim Cramer on 09/06/16.
+//  Copyright © 2016 Belajar NL. All rights reserved.
+//
+
+import Foundation
+
+class TopicMapper {
+    
+    let fieldNames = [
+        "ID",
+        "FileName",
+        "Publication",
+        "Chapter",
+        "GroupName",
+        "SortIndex",
+        "Title",
+        "SubTitle",
+        "Author",
+        "Publisher",
+        "PubDate",
+        "Icon",
+        "LastModified"
+    ]
+    
+    
+}
